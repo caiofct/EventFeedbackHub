d17521fdb388180173c2fa0df41a84c648eef649
Caio Teixeira <caiofct@gmail.com>
2025-05-12 18:47:13 -0300

    Add controller tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,8 +1,64 @@
 require "test_helper"
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @event = events(:one)
+    @feedback = feedbacks(:one)
+  end
+
   test "should get index" do
-    get events_index_url
+    get events_url
     assert_response :success
+    assert_select "h1", "Event Feedback Hub"
+    assert_select "form#new_feedback"
+    assert_select "#feedbacks"
+  end
+
+  test "should handle event filtering" do
+    get events_url, params: { event_id: @event.id }
+    assert_response :success
+    assert_select "#feedbacks" do
+      assert_select ".feedback" do
+        assert_select "div", @event.name
+      end
+    end
+  end
+
+  test "should handle rating filtering" do
+    get events_url, params: { rating: @feedback.rating }
+    assert_response :success
+    assert_select "#feedbacks" do
+      assert_select ".feedback" do
+        assert_select "div.text-yellow-500", /★{#{@feedback.rating}}/
+      end
+    end
+  end
+
+  test "should handle combined filtering" do
+    get events_url, params: { event_id: @event.id, rating: @feedback.rating }
+    assert_response :success
+    assert_select "#feedbacks" do
+      assert_select ".feedback" do
+        assert_select "div", @event.name
+        assert_select "div.text-yellow-500", /★{#{@feedback.rating}}/
+      end
+    end
+  end
+
+  test "should paginate feedbacks" do
+    # Create enough feedbacks to trigger pagination
+    15.times do |i|
+      Feedback.create!(
+        event: @event,
+        user_name: "User#{i}",
+        rating: rand(1..5),
+        comment: "Test comment #{i}"
+      )
+    end
+
+    get events_url
+    assert_response :success
+    assert_select ".pagination"
+    assert_select "#feedbacks .feedback", EventsController::PER_PAGE
   end
 end
--- test/controllers/feedbacks_controller_test.rb
+++ test/controllers/feedbacks_controller_test.rb
@@ -1,8 +1,55 @@
 require "test_helper"
 
 class FeedbacksControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get feedbacks_create_url
+  setup do
+    @event = events(:one)
+  end
+
+  test "should create feedback with valid attributes via html" do
+    assert_difference("Feedback.count") do
+      post feedbacks_path, params: {
+        feedback: {
+          event_id: @event.id,
+          user_name: "John Doe",
+          rating: 5,
+          comment: "Great event!"
+        }
+      }
+    end
+
+    assert_redirected_to events_path
+    assert_equal "Feedback was successfully submitted.", flash[:notice]
+  end
+
+  test "should create feedback with valid attributes via turbo stream" do
+    assert_difference("Feedback.count") do
+      post feedbacks_path, params: {
+        feedback: {
+          event_id: @event.id,
+          user_name: "John Doe",
+          rating: 5,
+          comment: "Great event!"
+        }
+      }, as: :turbo_stream
+    end
+
     assert_response :success
+    assert_match(/turbo-stream/, @response.media_type)
+  end
+
+  test "should not create feedback with invalid attributes" do
+    assert_no_difference("Feedback.count") do
+      post feedbacks_path, params: {
+        feedback: {
+          event_id: @event.id,
+          user_name: "", # invalid: blank user_name
+          rating: 5,
+          comment: "Great event!"
+        }
+      }
+    end
+
+    assert_redirected_to events_path
+    assert_not_empty flash[:alert]
   end
 end
